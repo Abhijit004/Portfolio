5d6182e8d8e989e70aea7edc472af1daa7e48347
Abhijit <artistrup07@gmail.com>
2024-08-03 00:38:07 +0530

    [UPDATE]: Mentor Experience Added

--- src/pages/experience/experience.jsx
+++ src/pages/experience/experience.jsx
@@ -8,6 +8,8 @@ var gdsc =
 var codeiiest =
     "Actively woking on designing and developing the official website for CodeIIEST, which will also feature a CP ranklist as a highlight."
 
+var mentorexp = "Mentored in a web development bootcamp, mainly in frontend development. Conducted multiple online sessions where I guided participants through live project builds, offering hands-on experience and practical insights into development processes."
+
 export default function Experience() {
     return (
         <div className="experience-section sec s4">
@@ -19,18 +21,25 @@ export default function Experience() {
             </div>
             <div className="exp-list">
                 <ExperienceCard
+                    company={"DevCamp"}
+                    role={"Mentor"}
+                    daterange={"June 2024 - July 2024"}
+                    loc={"Shibpur"}
+                    desc={mentorexp}
+                />
+                <ExperienceCard
+                    company={"CodeIIEST"}
+                    role={"Web Developer"}
+                    daterange={"Sept 2024 - Present"}
+                    loc={"Shibpur"}
+                    desc={codeiiest}
+                />
+                <ExperienceCard
                     company={"GDSC-IIEST"}
                     role={"Dev Team"}
                     daterange={"Sept 2024 - Present"}
                     loc={"Shibpur"}
                     desc={gdsc}
-                />
-                <ExperienceCard
-                    company={"CodeIIEST"}
-                    role={"Web Developer"}
-                    daterange={"Setp 2024 - Present"}
-                    loc={"Shibpur"}
-                    desc={codeiiest}
                 />
             </div>
         </div>
